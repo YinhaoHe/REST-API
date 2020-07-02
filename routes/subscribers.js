51e84ab73661001db325399e3dfd69b6b447b28f
YinhaoHe <yinhaoh@uci.edu>
2020-07-02 00:57:56 -0700

    Finalize server

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -13,8 +13,8 @@ router.get('/', async (req, res) => {
 })
 
 // Getting one
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/:id', getSubscriber, (req, res) => {
+    res.json(res.subscriber)
 })
 
 // Creating one
@@ -33,13 +33,45 @@ router.post('/', async (req, res) => {
 })
 
 // Updating one Patch: update info use gave Put: update all information
-router.patch('/:id', (req, res) => {
-    
+router.patch('/:id', getSubscriber, async (req, res) => {
+    if (req.body.name != null) {
+        res.subscriber.name = req.body.name
+    }
+    if (req.body.subscribedToChannel != null) {
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+    }
+    try {
+        const updatedSubscriber = await res.subscriber.save()
+        res.json(updatedSubscriber)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 })
 
 // Deleting one
-router.delete('/:id', (req, res) => {
-    
+router.delete('/:id', getSubscriber, async (req, res) => {
+    try {
+        await res.subscriber.remove()
+        res.json({ message: 'Deleted Subscriber' })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
+
+// Middleware
+async function getSubscriber(req, res, next) {
+    let subscriber
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null) {
+            return res.status(404).json({ message: 'Cannot find subscriber' })
+        }
+    } catch (err) {
+        return res.status(500)
+    }
+
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
